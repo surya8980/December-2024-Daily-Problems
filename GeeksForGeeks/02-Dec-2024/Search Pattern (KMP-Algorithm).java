b5e93eaec832cbd3b500d95c62fdd389ba7ce067
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-12-02 23:22:19 +0530

    Search Pattern (KMP-Algorithm).java

--- GeeksForGeeks/02-Dec-2024/Search Pattern (KMP-Algorithm).java
+++ GeeksForGeeks/02-Dec-2024/Search Pattern (KMP-Algorithm).java
@@ -1,1 +1,17 @@
+class Solution {
 
+    ArrayList<Integer> search(String pat, String txt) {
+        // your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n=pat.length();
+        for(int i=0;i<=txt.length()-n;i++)
+        {
+            if(pat.charAt(0)!=txt.charAt(i))
+                continue;
+            String str=txt.substring(i,n+i);
+            if(str.equals(pat))
+                ans.add(i);
+        }
+        return ans;
+    }
+}
