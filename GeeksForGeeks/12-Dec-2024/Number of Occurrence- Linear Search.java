c973bde08c932ee43a266f28b1220ead95224256
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-12-12 07:19:16 +0530

    Create Number of Occurrence- Linear Search.java

---
+++ GeeksForGeeks/12-Dec-2024/Number of Occurrence- Linear Search.java
@@ -0,0 +1,10 @@
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int count=0;
+        for(int i:arr){
+            if(i==target)count++;
+        }
+        return count;
+    }
+}
