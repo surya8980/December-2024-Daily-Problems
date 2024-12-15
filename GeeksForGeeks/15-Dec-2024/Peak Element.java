4fcac249f2ddb174dbef6a12911cea71247ad135
D Surya Prakash Reddy <125757799+surya8980@users.noreply.github.com>
2024-12-15 18:59:27 +0530

    Create Peak Element.java

---
+++ GeeksForGeeks/15-Dec-2024/Peak Element.java
@@ -0,0 +1,30 @@
+
+
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        int n = arr.length;
+        
+        if(n==1)return 0;
+        for (int i = 0; i < n; i++)
+        {      
+            if (i == 0)
+            {
+                if (arr[i] >= arr[i + 1])
+                    return i;
+            }
+            else if (i == n - 1)
+            {
+                if (arr[i] >= arr[i - 1])
+                    return i;
+            }
+            else
+            {
+                if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+                    return i;
+            }
+        }
+        return -1;
+    }
+}
